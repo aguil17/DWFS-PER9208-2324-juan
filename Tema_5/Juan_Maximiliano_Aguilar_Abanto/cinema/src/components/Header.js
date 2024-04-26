a7891932fc398789b29c9cf07999f16f51518295
Juan <aguil17@gmail.com>
2024-04-26 12:01:04 -0500

    enviar

--- Tema_5/Juan_Maximiliano_Aguilar_Abanto/cinema/src/components/Header.js
+++ Tema_5/Juan_Maximiliano_Aguilar_Abanto/cinema/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Header = () =>{
     return (
         <div className="principal__cabecera">
-            UNIR-CINEMA
+            UNIR-CINEMA2
         </div>
     );
 }
